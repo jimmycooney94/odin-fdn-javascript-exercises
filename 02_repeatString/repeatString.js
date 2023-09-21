ad4fd8f203403e189b9d20d3f14a027a10b17cc8
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-21 17:33:32 +0100

    completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inString, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let outString = "";
+    for (let i = 0; i < times; i++) {
+        outString = outString + inString;        
+    }
+    return outString;
 };
 
 // Do not edit below this line
