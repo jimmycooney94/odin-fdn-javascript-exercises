5bd24edce8fcd1c65f36c7d88f5d92c483a04d70
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-22 13:41:48 +0100

    completed exercise 4 all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...valuesToRemove) {
+    for (let i = 0; i < valuesToRemove.length; i++) {
+        for (let j = 0; j < theArray.length;j++) {
+            if (theArray[j] === valuesToRemove[i]) {
+                theArray.splice(j,1);
+            }
+        }
+    }
+    return theArray;
 };
+
+console.log(removeFromArray(["a","b","c"],"b"));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
