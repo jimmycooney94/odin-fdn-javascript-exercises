60ead8d6998f1ca05c38db72dd7e220ed927a01d
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-22 14:18:47 +0100

    completed exercise 7 all tests pass

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function round1dp (val) {
+  return Math.round(10*val)/10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  return round1dp((fahrenheit - 32) * 5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return round1dp((celsius * 1.8) + 32);
 };
 
 // Do not edit below this line
