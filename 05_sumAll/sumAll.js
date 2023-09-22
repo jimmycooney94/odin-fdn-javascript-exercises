8beb870e5aa6e690a82cb9fc491deeeb8994082a
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-22 13:59:20 +0100

    completed exercise 5 all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0) return "ERROR";
+    if (typeof number1 !== "number" || typeof number2 !== "number") return "ERROR";
+    if (number1 % 1 !== 0 || number2 % 1 !== 0) return "ERROR";
 
+    const minNum = Math.min(number1,number2);
+    const maxNum = Math.max(number1,number2);
+    const numSteps = maxNum - minNum + 1; 
+
+    let total = 0;
+    for (let currentNum = minNum; currentNum <= maxNum; currentNum++) {
+        total += currentNum;
+    }
+    
+    return total;
 };
 
 // Do not edit below this line
