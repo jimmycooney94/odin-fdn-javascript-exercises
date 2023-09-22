b6174c93428e65e5c99b400eb37a3bd0e2c6cece
jimmycooney94 <52164695+jimmycooney94@users.noreply.github.com>
2023-09-22 08:57:39 +0100

    exercise 3 complete - but not tested

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (forwardString) {
+  let reversedString = "";
+  const stringLength = forwardString.length;
+  for (let i = 0; i < stringLength; i++) {
+    reversedString += forwardString.charAt(stringLength - 1 - i);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
