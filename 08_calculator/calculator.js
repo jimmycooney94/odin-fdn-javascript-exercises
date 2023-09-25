bba50100a7bbef1bb8fcb50a44960666a089e515
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-25 10:32:18 +0100

    completed exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function (a, b) {
+  return a - b;
 
 };
 
-const power = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((total, current) => total + current, 0);
 };
 
-const factorial = function() {
-	
+const multiply = function (nums) {
+  return nums.reduce((product, current) => product * current, 1);
+};
+
+const power = function (num, order) {
+  return num ** order;
+};
+
+const factorial = function (num) {
+  let product = 1;
+  for (let i = 1; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
