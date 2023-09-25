54ea004480dfecb5b6b12fffa6053afaccc60957
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-25 11:40:31 +0100

    completed exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (nums) {
+    if (parseInt(nums) < 0) return "OOPS";
+    const numArr = [];
+    for (let i = 0; i < nums; i++) {
+        if (numArr.length < 2) numArr.push(1);
+        else numArr.push(numArr[i - 2] + numArr[i - 1]);
+    }
+    return numArr[numArr.length - 1];
 };
 
 // Do not edit below this line
