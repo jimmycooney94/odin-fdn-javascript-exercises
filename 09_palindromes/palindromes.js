d52ade3a48aa4a7796cfa2c81adcf4cbcd6d2c73
Jimmy Cooney <jimmycooney@hotmail.co.uk>
2023-09-25 11:24:25 +0100

    completed 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedStr === processedStr.split("").reverse().join("");
 };
 
+/* 
+
+// OLD SOLUTION - BEFORE USING .replace(a,b);
+
+const palindromes = function (fwdStr) {
+    const strToTest = toAlphaNum(fwdStr).toLowerCase();
+    return strToTest === strToTest.split("").reverse().join("");
+};
+
+
+const toAlphaNum = function (str) {
+    return str.split("").filter(char => isAlphaNum(char)).join("");
+};
+
+const isAlphaNum = function (char) {
+    const regex = /[A-Za-z0-9]/;
+    return regex.test(char);
+
+}
+*/
+
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = {
+    palindromes,
+    //toAlphaNum,
+    //isAlphaNum
+};
--- 09_palindromes/palindromes.spec.js
+++ 09_palindromes/palindromes.spec.js
@@ -1,28 +1,64 @@
 const palindromes = require('./palindromes')
 
+// describe('isAlphaNum', () => {
+//   test('works with lowercase', () => {
+//     expect(palindromes.isAlphaNum("a")).toBe(true);
+//   });
+//   test('works with uppercase', () => {
+//     expect(palindromes.isAlphaNum("A")).toBe(true);
+//   });
+//   test('works with number', () => {
+//     expect(palindromes.isAlphaNum("5")).toBe(true);
+//   });
+//   test('works with non-alpha character', () => {
+//     expect(palindromes.isAlphaNum("@")).toBe(false);
+//   });
+//   test('works with space', () => {
+//     expect(palindromes.isAlphaNum(" ")).toBe(false);
+//   });
+// });
+
+// describe('toAlphaNum', () => {
+//   test('works with single words', () => {
+//     expect(palindromes.toAlphaNum('racecar')).toBe("racecar");
+//   });
+//   test('works with punctuation ', () => {
+//     expect(palindromes.toAlphaNum('racecar!')).toBe("racecar");
+//   });
+//   test('works with upper-case letters ', () => {
+//     expect(palindromes.toAlphaNum('Racecar!')).toBe("Racecar");
+//   });
+//   test('works with multiple words', () => {
+//     expect(palindromes.toAlphaNum('A car, a man, a maraca.')).toBe("Acaramanamaraca");
+//   });
+//   test('works with numbers in a string', () => {
+//     expect(palindromes.toAlphaNum('rac3e3car')).toBe("rac3e3car");
+//   });
+// })
+
 describe('palindromes', () => {
   test('works with single words', () => {
-    expect(palindromes('racecar')).toBe(true);
+    expect(palindromes.palindromes('racecar')).toBe(true);
   });
-  test.skip('works with punctuation ', () => {
-    expect(palindromes('racecar!')).toBe(true);
+  test('works with punctuation ', () => {
+    expect(palindromes.palindromes('racecar!')).toBe(true);
   });
-  test.skip('works with upper-case letters ', () => {
-    expect(palindromes('Racecar!')).toBe(true);
+  test('works with upper-case letters ', () => {
+    expect(palindromes.palindromes('Racecar!')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
-    expect(palindromes('A car, a man, a maraca.')).toBe(true);
+  test('works with multiple words', () => {
+    expect(palindromes.palindromes('A car, a man, a maraca.')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
-    expect(palindromes('Animal loots foliated detail of stool lamina.')).toBe(true);
+  test('works with multiple words', () => {
+    expect(palindromes.palindromes('Animal loots foliated detail of stool lamina.')).toBe(true);
   });
-  test.skip('doesn\'t just always return true', () => {
-    expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
+  test('doesn\'t just always return true', () => {
+    expect(palindromes.palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
   });
-  test.skip('works with numbers in a string', () => {
-    expect(palindromes('rac3e3car')).toBe(true);
+  test('works with numbers in a string', () => {
+    expect(palindromes.palindromes('rac3e3car')).toBe(true);
   });
-  test.skip('works with unevenly spaced numbers in a string', () => {
-    expect(palindromes('r3ace3car')).toBe(false);
+  test('works with unevenly spaced numbers in a string', () => {
+    expect(palindromes.palindromes('r3ace3car')).toBe(false);
   });
 });
